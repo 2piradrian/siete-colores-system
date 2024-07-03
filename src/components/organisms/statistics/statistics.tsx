38eb7df9869fd2c723c7ea0933425fcaa75dc055
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-03 18:17:02 -0300

    div to section

--- src/components/organisms/statistics/statistics.tsx
+++ src/components/organisms/statistics/statistics.tsx
@@ -7,11 +7,11 @@ export default function Statistics() {
 	};
 
 	return (
-		<div className="container" style={{"justifyContent": "center"}}>
+		<section className="container" style={{"justifyContent": "center"}}>
 			<h2>Bienvenido,</h2>
 			<h1 className={style.h1}>Siete Colores</h1>
 			<h3 className={style.h3}>Estadísticas del mes de {getMonth()}</h3>
 			<Stats />
-		</div>
+		</section>
 	);
 }
