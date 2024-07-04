9a356b706c877cd659fe231ef3c6b7096820032d
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-04 12:57:58 -0300

    refactor: Update Products component to use ProductsList

---
+++ src/components/organisms/product-list/products-list.tsx
@@ -0,0 +1,22 @@
+import { allProducts } from "../../../data-structures/tables";
+import { Product } from "../../../types/types";
+import ProductsTable from "../../molecules/products-table/products-table";
+import style from "./style.module.css";
+
+type Props = {
+    products: Product[];
+    onClick: (code: string) => void;
+};
+
+export default function ProductsList({ products, onClick }: Props) {
+    
+    return (
+        <div className={style.container}>
+            <ProductsTable 
+                table={allProducts}
+                products={products} 
+                onClick={onClick} 
+            />
+        </div>
+    );
+}
--- src/routes/products/products.tsx
+++ src/routes/products/products.tsx
@@ -1,22 +1,17 @@
-import ProductsTable from "../../components/molecules/products-table/products-table";
 import SearchProducts from "../../components/molecules/search-products/search-products";
-import { allProducts } from "../../data-structures/tables";
+import ProductsList from "../../components/organisms/product-list/products-list";
 import useProducts from "../../hooks/useProducts";
 import AppLayout from "../../layout/app-layout";
 import ContainerLayout from "../../layout/container-layout";
 
 export default function Products() {
-    const { products, setSearch  } = useProducts();
+    const { products, setSearch } = useProducts();
     
     return (
         <AppLayout>
             <ContainerLayout title="Productos">
                 <SearchProducts setSearch={setSearch}/>
-                <ProductsTable 
-                    table={allProducts}
-                    products={products} 
-                    onClick={(code: string) => {console.log(code)}} 
-                />
+                <ProductsList products={products} onClick={(code: string) => {console.log(code)}} />
             </ContainerLayout>
         </AppLayout>
     );
