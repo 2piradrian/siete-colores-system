beae77a01f8543361b0c7eac2806b9b1fccad971
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-07-07 13:58:58 -0300

    confirmation added

--- src/hooks/useBudget.tsx
+++ src/hooks/useBudget.tsx
@@ -31,6 +31,8 @@ export default function useBudget() {
 			if (budget.products.length === 0 || !budget.client) {
 				throw new Error("No se puede crear un presupuesto sin productos o cliente");
 			}
+
+			alert("Creando presupuesto...")
 			await httpClient.post("/budgets/create", { products: budget.products, client: budget.client, discount: budget.discount });
 			
 			alert("Presupuesto creado con éxito");
