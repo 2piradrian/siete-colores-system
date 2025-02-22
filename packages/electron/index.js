759b6b847d95e9cd5a41e862bf074a206b3cf0c7
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-02-22 12:47:55 -0300

    project init

---
+++ packages/electron/index.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const { spawn } = require("child_process");
+
+let window;
+let serverProcess;
+
+app.whenReady().then(() => {
+
+  serverProcess = spawn("node", [path.join(__dirname, "../server/index.js")], {
+    stdio: "ignore",
+    detached: true
+  });
+  serverProcess.unref();
+
+  Menu.setApplicationMenu(null);
+
+  window = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    minWidth: 1280,
+    minHeight: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  window.loadURL("http://localhost:5173");
+
+  window.on("closed", () => {
+    window = null;
+    if (serverProcess) serverProcess.kill();
+  });
+
+});
+
+app.on("window-all-closed", () => {
+  if (serverProcess) serverProcess.kill();
+  if (process.platform !== "darwin") app.quit();
+});
